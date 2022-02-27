8950dd76f4fe5596a26b5c16cd6962a81464443c
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-02-28 04:24:56 +0700

    init

---
+++ src/routes.js
@@ -0,0 +1,43 @@
+const {
+   addNoteHandler,
+   getAllNotesHandler,
+   getNoteByIdHandler,
+   editNoteByIdHandler,
+   deletNoteByIdHandler
+} = require("./handler")
+
+const routes = [{
+      method: 'GET',
+      path: '/',
+      handler: (request, h) => {
+         return "Notes App Backend"
+      },
+   },
+   {
+      method: 'POST',
+      path: '/notes',
+      handler: addNoteHandler,
+   },
+   {
+      method: 'GET',
+      path: '/notes',
+      handler: getAllNotesHandler,
+   },
+   {
+      method: 'GET',
+      path: '/notes/{id}',
+      handler: getNoteByIdHandler,
+   },
+   {
+      method: 'PUT',
+      path: '/notes/{id}',
+      handler: editNoteByIdHandler,
+   },
+   {
+      method: 'DELETE',
+      path: '/notes/{id}',
+      handler: deletNoteByIdHandler,
+   },
+]
+
+module.exports = routes
